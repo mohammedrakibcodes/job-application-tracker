2a4ec6e9cbdf50fbf1229f66c2d0943fbb510ba9
mohammedrakibcodes <mohammedrakibhossain02@gmail.com>
2026-02-23 19:29:32 +0600

    Add job data setup

---
+++ Js/script.js
@@ -0,0 +1,91 @@
+// All Job data =>
+
+let jobList = [
+  {
+    id: 1,
+    companyName: "TechNova Solutions",
+    position: "Frontend Developer",
+    location: "Remote",
+    type: "Full-time",
+    salary: "$1200/month",
+    description: "Build responsive UI using modern JavaScript frameworks.",
+    status: "all",
+  },
+
+  {
+    id: 2,
+    companyName: "CloudSync Ltd",
+    position: "Backend Developer",
+    location: "Dhaka",
+    type: "Full-time",
+    salary: "$1500/month",
+    description: "Develop REST APIs and manage server architecture.",
+    status: "all",
+  },
+
+  {
+    id: 3,
+    companyName: "PixelCraft Studio",
+    position: "UI Designer",
+    location: "Remote",
+    type: "Contract",
+    salary: "$900/month",
+    description: "Design user interfaces for web applications.",
+    status: "all",
+  },
+
+  {
+    id: 4,
+    companyName: "SecureNet Systems",
+    position: "Cyber Security Analyst",
+    location: "Chittagong",
+    type: "Full-time",
+    salary: "$1800/month",
+    description: "Monitor and secure enterprise network systems.",
+    status: "all",
+  },
+
+  {
+    id: 5,
+    companyName: "DataMind AI",
+    position: "Machine Learning Engineer",
+    location: "Remote",
+    type: "Full-time",
+    salary: "$2000/month",
+    description: "Develop AI models and improve prediction systems.",
+    status: "all",
+  },
+
+  {
+    id: 6,
+    companyName: "BrightApps",
+    position: "Mobile App Developer",
+    location: "Sylhet",
+    type: "Full-time",
+    salary: "$1300/month",
+    description: "Build Android and iOS applications.",
+    status: "all",
+  },
+
+  {
+    id: 7,
+    companyName: "WebCore Agency",
+    position: "WordPress Developer",
+    location: "Remote",
+    type: "Freelance",
+    salary: "$800/month",
+    description: "Develop and maintain WordPress websites.",
+    status: "all",
+  },
+
+  {
+    id: 8,
+    companyName: "FinEdge Corp",
+    position: "Software Engineer",
+    location: "Dhaka",
+    type: "Full-time",
+    salary: "$1700/month",
+    description: "Maintain scalable financial software systems.",
+    status: "all",
+  },
+];
